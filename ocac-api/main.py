c26582746dfda3721d683829d78e9964798b7d85
DoubleWa <ocheriaf@gmail.com>
2021-03-19 02:03:53 +0100

    Update some logs at the launch of the api

--- ocac-api/main.py
+++ ocac-api/main.py
@@ -4,6 +4,7 @@ from ocac_api.config import app_settings
 
 if __name__ == "__main__":
     import logging
-    logging.info("API Starting")
+    logging.info("API Starting with those app settings")
+    
     logging.info(app_settings)
     uvicorn.run("ocac_api.api:api", reload=app_settings.reload, host="0.0.0.0", port=5000)
