46fade1beaa8c19d23714b4c87aa52332debc5db
DoubleWa <ocheriaf@gmail.com>
2021-03-19 01:54:59 +0100

    Add some logs at the launch of the api

--- ocac-api/main.py
+++ ocac-api/main.py
@@ -3,4 +3,6 @@ import ocac_api.repository.sql.SQLRepository
 from ocac_api.config import app_settings
 
 if __name__ == "__main__":
+    import logging
+    logging.info("API Starting")
     uvicorn.run("ocac_api.api:api", reload=app_settings.reload, host="0.0.0.0", port=5000)
